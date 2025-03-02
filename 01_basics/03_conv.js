71af8abd036806cd78b253620d62e712fb209b25
Srush0641 <srushty60@gmail.com>
2025-03-02 13:15:55 +0000

    comparision

---
+++ 01_basics/03_conv.js
@@ -0,0 +1,22 @@
+// let score = "33abc"
+
+// let score = null
+
+let score = "srushty"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInnum = Number(score)
+console.log(typeof valueInnum)
+console.log(valueInnum)
+
+let isLoggedIn = "srushty"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+let k= 33
+let stringnum = String(k)
+console.log(stringnum);
+console.log(typeof stringnum)
