6f1e73db1d7e7627c57495c61d3de265e9e02903
Programmer#206 <srushty60@gmail.com>
2025-05-04 10:10:37 +0000

    datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,35 @@
+// primitive data type = call by value (copy m changes honge)
+// 7  types : String ,Boolean,Number,Null(ekdum khali),undefined,Symbol,Bigint
+
+const score = 100
+const scoren = 100.5
+
+const isloggedin = true
+const tempoutside = null
+
+let username; //undefined
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id == anotherid)
+
+// const bignumber = 3456745645677543456n
+// console.log(typeof bignumber)
+
+const heros = ["shaktiman","naagraj","doga"] //array
+{
+    name:"srushty"
+    age:19
+}//object
+
+const myfunc = function(){
+    console.log("hello")
+}
+
+// Non primitive / Reference type
+
+// Array , Objects,Functions
+
+// it is dynamically typed
+
