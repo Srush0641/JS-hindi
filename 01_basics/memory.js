baaac35be3a38191cab0b69344b8d348d403a4e5
Programmer#206 <srushty60@gmail.com>
2025-05-04 10:21:43 +0000

    memory

---
+++ 01_basics/memory.js
@@ -0,0 +1,21 @@
+// stack(primitive) = copy milta h
+// heap(non primitive) = original value m change hota h
+
+let myyoutube_name = "starting"
+
+let another_name = myyoutube_name
+another_name = "smile"
+console.log (another_name)
+console.log(myyoutube_name)
+
+let userone ={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+
+let usertwo = userone
+
+usertwo.email = "srushty@gmail.com"
+
+console.log(userone.email)
+console.log(usertwo.email)
